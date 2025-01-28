05c97b3353ec57dcaa2eee4f2892cf33e6aee9f4
maythushar5sn <maythushar-music@thepiratebay.cloud>
2025-01-28 15:55:39 +0630

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 900))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @BRANDRD_ROBOT on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", "1318826936"))
+OWNER_ID = int(getenv("OWNER_ID", "5821905026"))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/maythushar-music/CaiLinXMusic",
+    "https://github.com/maythushar5sn/CaiLinXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
